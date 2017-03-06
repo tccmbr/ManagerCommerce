07294e29c97e33f525c0d04cd320b5deb01cbdc5
Ewerton Oliveira <ewerton.tccmbr@gmail.com>
2017-03-06 14:17:29 -0300

    Adicionando estrutura inicial do projeto.

---
+++ src/br/com/ewertonolivera/managercommerce/ui/ManagerCommerce.java
@@ -0,0 +1,10 @@
+package br.com.ewertonolivera.managercommerce.ui;
+
+/**
+ * Manager Commerce
+ * <p>
+ * Author: Ewerton Oliveira
+ * E-mail: ewerton@intelnet.com.br
+ */
+public class ManagerCommerce {
+}
