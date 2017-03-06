03980430da9deb831cba44e2ded696c0f0e75b8b
Ewerton Oliveira <ewerton.tccmbr@gmail.com>
2017-03-06 14:18:31 -0300

    Adicionando estrutura inicial do projeto.

--- src/br/com/ewertonolivera/managercommerce/ui/ManagerCommerce.java
+++ src/br/com/ewertonolivera/managercommerce/ui/ManagerCommerce.java
@@ -1,10 +1,25 @@
 package br.com.ewertonolivera.managercommerce.ui;
 
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Manager Commerce
  * <p>
  * Author: Ewerton Oliveira
  * E-mail: ewerton@intelnet.com.br
  */
-public class ManagerCommerce {
+public class ManagerCommerce extends JFrame {
+
+    public static void main(String[] args) {
+        new ManagerCommerce().montaTela();
+    }
+
+    private void montaTela() {
+        setTitle("Manager Commerce");
+        setMinimumSize(new Dimension(500,500));
+        pack();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
 }
