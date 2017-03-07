d2df2d137b68b980ea7247451a0e14af6edc97ce
Ewerton Oliveira <ewerton.tccmbr@gmail.com>
2017-03-07 15:24:49 -0300

    Refatorando a classe ManagerCommerce para exibir um menu e organizando os métodos de cada tela.

--- src/br/com/ewertonolivera/managercommerce/ui/ManagerCommerce.java
+++ src/br/com/ewertonolivera/managercommerce/ui/ManagerCommerce.java
@@ -16,12 +16,46 @@ public class ManagerCommerce extends JFrame {
     }
 
     private void montaTela() {
+        preparaMenu();
+        preparaPainelPrincipal();
+        preparaJanela();
+    }
+
+    private void preparaJanela() {
         setTitle("Manager Commerce");
-        setMinimumSize(new Dimension(500,500));
+        setMinimumSize(new Dimension(800,500));
+        java.net.URL logoUrl = getClass().getResource("icone.png");
+        setIconImage(Toolkit.getDefaultToolkit().getImage(logoUrl));
         pack();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
-        preparaCadastroProduto();
+    }
+
+    private void preparaMenu() {
+        // barra de menu
+        JMenuBar menuBar = new JMenuBar();
+
+        // menu produto
+        JMenu produtoMenu = new JMenu("Produto");
+
+        // itens do menu produto
+        JMenuItem cadastroProdutoItem = new JMenuItem("Cadastro");
+        cadastroProdutoItem.addActionListener(e -> preparaCadastroProduto());
+
+        // adiciona os componentes na tela
+        produtoMenu.add(cadastroProdutoItem);
+        menuBar.add(produtoMenu);
+        setJMenuBar(menuBar);
+        validate();
+    }
+
+    private void preparaPainelPrincipal() {
+        java.net.URL logoUrl = getClass().getResource("icone.png");
+        ImageIcon logo = new ImageIcon(logoUrl);
+        JLabel image = new JLabel("", logo, JLabel.CENTER);
+
+        add(image);
+        validate();
     }
 
     private void preparaCadastroProduto(){
