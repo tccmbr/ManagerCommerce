614795d2db99fc6310545593884cd06d3a240478
Lucas Anselmo <lucasanselmo01@hotmail.com>
2017-03-08 09:51:00 -0300

    Adicionado o formulario e os botões na tela de cadastro de produto.

--- src/br/com/ewertonolivera/managercommerce/ui/CadastroProdutoUI.java
+++ src/br/com/ewertonolivera/managercommerce/ui/CadastroProdutoUI.java
@@ -14,9 +14,11 @@ class CadastroProdutoUI extends JPanel {
     CadastroProdutoUI () {
         setLayout(new BoxLayout(CadastroProdutoUI.this, BoxLayout.Y_AXIS));
         titulo();
+        formulario();
+        botoes();
     }
 
-    private void titulo(){
+    private void titulo() {
         JPanel painel = new JPanel();
         painel.setLayout(new BorderLayout());
         painel.setBackground(Color.darkGray);
@@ -30,5 +32,78 @@ class CadastroProdutoUI extends JPanel {
 
         painel.add(titulo, BorderLayout.NORTH);
         add(painel);
+    }
+
+    private void formulario() {
+        JPanel form = new JPanel();
+        form.setLayout(new BoxLayout(form,BoxLayout.Y_AXIS));
+
+        JPanel boxNome = new JPanel();
+        boxNome.setLayout(new BoxLayout(boxNome, BoxLayout.X_AXIS));
+        boxNome.setMaximumSize(new Dimension(500, 28));
+        JLabel nomeLabel = new JLabel("Nome:", null, JLabel.RIGHT);
+        JTextField caixaDeTexto1 = new JTextField();
+        nomeLabel.setPreferredSize(new Dimension(110,28));
+        nomeLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+
+        JPanel boxQuantidade = new JPanel();
+        boxQuantidade.setLayout(new BorderLayout());
+        boxQuantidade.setMaximumSize(new Dimension(500, 28));
+        JLabel quantidadeLabel = new JLabel("Quantidade:", null, JLabel.RIGHT);
+        JTextField caixadetexto2 = new JTextField();
+        quantidadeLabel.setPreferredSize(new Dimension(110,28));
+        quantidadeLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+
+        JPanel boxValor = new JPanel();
+        boxValor.setLayout(new BorderLayout());
+        boxValor.setMaximumSize(new Dimension(500, 28));
+        JLabel valorLabel = new JLabel("Valor:", null, JLabel.RIGHT);
+        JTextField caixadetexto3 = new JTextField();
+        valorLabel.setPreferredSize(new Dimension(110,28));
+        valorLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+
+        JPanel boxDescricao = new JPanel();
+        boxDescricao.setLayout(new BorderLayout());
+        boxDescricao.setMaximumSize(new Dimension(500, 28));
+        JLabel descricaoLabel = new JLabel("Descrição:", null, JLabel.RIGHT);
+        JTextField caixadetexto4 = new JTextField();
+        descricaoLabel.setPreferredSize(new Dimension(110,28));
+        descricaoLabel.setFont(new Font("Verdana", Font.BOLD, 16));
+
+        add(Box.createRigidArea(new Dimension(0,50)));
+        boxNome.add(nomeLabel);
+        boxNome.add(caixaDeTexto1);
+        add(boxNome);
+
+        add(Box.createRigidArea(new Dimension(0,15)));
+        boxQuantidade.add(quantidadeLabel, BorderLayout.WEST);
+        boxQuantidade.add(caixadetexto2, BorderLayout.CENTER);
+        add(boxQuantidade);
+
+        add(Box.createRigidArea(new Dimension(0,15)));
+        boxValor.add(valorLabel, BorderLayout.WEST);
+        boxValor.add(caixadetexto3, BorderLayout.CENTER);
+        add(boxValor);
+
+        add(Box.createRigidArea(new Dimension(0,15)));
+        boxDescricao.add(descricaoLabel, BorderLayout.WEST);
+        boxDescricao.add(caixadetexto4, BorderLayout.CENTER);
+        add(boxDescricao);
+    }
+
+    private void botoes () {
+        JPanel boxBotoes = new JPanel();
+        boxBotoes.setLayout(new BoxLayout(boxBotoes, BoxLayout.X_AXIS));
+        boxBotoes.setMaximumSize(new Dimension(500, 28));
+
+        JButton botaoCadastrar = new JButton("Cadastrar");
+        JButton botaoLimpar = new JButton("Limpar");
+
+        boxBotoes.add(Box.createRigidArea(new Dimension(200,0)));
+        boxBotoes.add(botaoCadastrar);
+        boxBotoes.add(Box.createRigidArea(new Dimension(20,0)));
+        boxBotoes.add(botaoLimpar);
+        add(Box.createRigidArea(new Dimension(0,50)));
+        add(boxBotoes);
     }
 }
